f4cd1ff036016cc161cc1672ee65157de89f95b5
Sebik3012Sk <sebik-kucera@hotmail.com>
2024-09-15 13:48:12 +0200

    Models & Controllers/ListPost.xaml - .cs & Models - Post.cs

---
+++ ArsMediaApp/Controllers/LIstPost.xaml.cs
@@ -0,0 +1,45 @@
+namespace ArsMediaApp.Controllers;
+using ArsMediaApp.Database;
+using ArsMediaApp.Models;
+using System.Collections.ObjectModel;
+using ArsMediaApp.Pages;
+
+public partial class LIstPost : ContentView
+{
+    ObservableCollection<Post> posts;
+    Connection connection;
+
+    public LIstPost()
+	{
+		InitializeComponent();
+        posts = new ObservableCollection<Post>();
+        connection = new Connection();
+        listView_posts.ItemsSource = posts;
+    }
+
+
+    private void LoadContent_Event(object sender, EventArgs e)
+    {
+        var data_posts = connection.connectDb("http://localhost:59281/get-post");
+        foreach (var post in data_posts)
+        {
+            posts.Add(new Post
+            {
+                PostName = post["post_name"].ToString(),
+                PostContent = post["post_content"].ToString(),
+                Likes = post["likes"],
+                Description = post["description"].ToString()
+            });
+        }
+    }
+
+    private async void SelectedPost_Event(object sender, SelectedItemChangedEventArgs e)
+    {
+        var selectedPost = e.SelectedItem as Post;
+
+        if(selectedPost != null)
+        {
+            await Navigation.PushAsync(new PostDetail(selectedPost));
+        }
+    }
+}
---
+++ ArsMediaApp/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArsMediaApp.Models
+{
+    public class Post
+    {
+        public string PostName { get; set; }
+        public int Likes { get; set; }
+        public string PostContent { get; set; }
+        public string Description { get; set; }
+    }
+}
